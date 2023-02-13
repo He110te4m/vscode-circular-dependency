9a6f0e9091f1ae06f61cd29779e6c34827d63c63
He110te4m <me@he110.site>
2023-02-13 10:25:05 +0800

    feat: extra diagnostic service

--- apps/vscode-circular-dependency/src/circularDependencies/index.ts
+++ apps/vscode-circular-dependency/src/circularDependencies/index.ts
@@ -1,17 +1,9 @@
-import { type DiagnosticCollection, type Disposable, type Memento, type TextDocument, type Uri, languages, window, workspace } from 'vscode'
-import { debounce } from 'debounce'
+import { type Disposable, type Memento, languages } from 'vscode'
 import { isAllowedCircularDependency } from '../helpers/config'
-import type { CacheStoreType } from './types'
-import { detectCircularDependencies } from './detectCircularDependencies'
-import { resolveCircularDependencies } from './resolveCircularDependencies'
-import { createDiagnosticsByDependencies } from './createDiagnosticsByDependencies'
+import type { AllCacheCollections } from './types'
+import { registerDiagnosticService } from './services/diagnostic'
 
 const disposables: Disposable[] = []
-
-interface AllCacheCollections {
-  diagnosticCacheStore: DiagnosticCollection
-  dependenciesCacheStore: CacheStoreType
-}
 
 export function useCircularDependenciesDetection(cacheMap: Memento): Disposable[] {
   if (isAllowedCircularDependency()) {
@@ -29,9 +21,9 @@ export function useCircularDependenciesDetection(cacheMap: Memento): Disposable[
   return [
     collection,
     cleanDisposables(),
-    registerForActivationEventListener(cacheCollectoions),
-    registerFileContentChangeEventListener(cacheCollectoions),
-  ]
+  ].concat(
+    registerDiagnosticService(cacheCollectoions),
+  )
 }
 
 function cleanDisposables() {
@@ -42,56 +34,6 @@ function cleanDisposables() {
       })
     },
   }
-}
-
-function registerForActivationEventListener(opts: AllCacheCollections) {
-  const { document: doc } = window.activeTextEditor ?? {}
-  if (doc) {
-    registerCircularDependencyActions(doc, opts)
-  }
-
-  return window.onDidChangeActiveTextEditor((ev) => {
-    if (!ev) {
-      return
-    }
-
-    registerCircularDependencyActions(ev.document, opts)
-  })
-}
-
-function registerFileContentChangeEventListener(opts: AllCacheCollections) {
-  const updateFn = debounce((doc: TextDocument) => registerCircularDependencyActions(doc, opts), 100)
-
-  return workspace.onDidChangeTextDocument((ev) => {
-    // TODO: Use full update temporarily, then optimize performance later to do throttling
-    updateFn(ev.document)
-  })
-}
-
-function registerCircularDependencyActions(doc: TextDocument, opts: AllCacheCollections) {
-  const { uri } = doc
-  const { diagnosticCacheStore, dependenciesCacheStore } = opts
-
-  // clear cache ang error
-  deleteCacheByUri(uri, opts)
-
-  // update diagnostic collection
-  const diagnosticsList = createDiagnosticsByDependencies(
-    getFormatterCircularDependencies(dependenciesCacheStore, doc),
-  )
-  diagnosticCacheStore.set(uri, diagnosticsList)
-}
-
-function getFormatterCircularDependencies(cacheMap: CacheStoreType, doc: TextDocument) {
-  return resolveCircularDependencies(
-    doc,
-    detectCircularDependencies(doc.uri.fsPath, cacheMap),
-  )
-}
-
-function deleteCacheByUri(uri: Uri, { diagnosticCacheStore, dependenciesCacheStore }: AllCacheCollections) {
-  diagnosticCacheStore.delete(uri)
-  dependenciesCacheStore.update(uri.fsPath, undefined)
 }
 
 function cleanCacheMap({ diagnosticCacheStore, dependenciesCacheStore }: AllCacheCollections) {
--- apps/vscode-circular-dependency/src/circularDependencies/types.ts
+++ apps/vscode-circular-dependency/src/circularDependencies/types.ts
@@ -1,5 +1,5 @@
 import type { CheckCommentEffectivenessOptions } from '@circular-dependency/utils/libs/comment'
-import type { Memento, Range } from 'vscode'
+import type { DiagnosticCollection, Memento, Range } from 'vscode'
 
 export type CacheStoreType = Memento
 
@@ -11,3 +11,8 @@ export interface FormatterCircularDependencies {
 }
 
 export type CommentChars = CheckCommentEffectivenessOptions['commentChars'][number]
+
+export interface AllCacheCollections {
+  diagnosticCacheStore: DiagnosticCollection
+  dependenciesCacheStore: CacheStoreType
+}
